4a8220fd887e03c01f00f4b868a84afa5fe40c3d
Mahmoud Tokura <mahmoudbintokura@gmail.com>
2017-12-04 22:11:33 +0100

    Added email functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { WpProvider } from '../providers/wp/wp';
+import { EmailComposer } from '@ionic-native/email-composer';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import { WpProvider } from '../providers/wp/wp';
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
+    EmailComposer,
     WpProvider
   ]
 })
--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { EmailComposer } from '@ionic-native/email-composer';
 
 /**
  * Generated class for the ContactPage page.
@@ -15,11 +16,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private emailComposer: EmailComposer) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ContactPage');
+  }
+
+  sendEmail(){
+    let email = {
+      to: "mahmoudbintokura@gmail.com",
+      cc: "imopumping@gmail.com",
+      subject: "Customer Service from App",
+      body: "Testing email from app"
+    };
+    this.emailComposer.open(email);
   }
 
 }
